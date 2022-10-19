35388ef6ebbf3c628147ba499749d373735c6207
Carol Ferreira <developer.anacarolina@gmail.com>
2022-10-19 18:53:25 -0300

    feat: add cover github

--- App.tsx
+++ App.tsx
@@ -9,21 +9,19 @@ import {
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
 
-// export default function App() {
-//   const [fontsLoaded] = useFonts({
-//     Inter_400Regular,
-//     Inter_700Bold,
-//   });
-//   return (
-//     <>
-//       <StatusBar
-//         barStyle={"light-content"}
-//         backgroundColor="transparent"
-//         translucent
-//       />
-//       {fontsLoaded ? <Home /> : <Load />}
-//     </>
-//   );
-// }
-
-export { default } from "./storybook";
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+  return (
+    <>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Load />}
+    </>
+  );
+}
